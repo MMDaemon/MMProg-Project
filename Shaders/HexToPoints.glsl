5440dd5b0ff3137cd8c04b86e08781a845d9fcff
MMDaemon <mettenleitermatthias@web.de>
2017-02-21 20:13:12 +0100

    Added HexToPoints and did some refactoring

--- Shaders/HexToPoints.glsl
+++ Shaders/HexToPoints.glsl
@@ -1,7 +1,6 @@
 uniform vec2 iResolution;
 uniform float iGlobalTime;
-uniform sampler2D texLastFrame;
-uniform float frequency;
+uniform float dist;
 
 struct VoronoiResult
 {
@@ -70,22 +69,6 @@ VoronoiResult voronoi( in vec2 pos )
     return VoronoiResult( borderDist, nearestPointDir );
 }
 
-vec3 Rings(vec2 pos)
-{
-	float aspect = iResolution.y/iResolution.x;
-	pos = (pos-vec2(0.5,0.5*aspect))*2;
-	
-	
-	float dist = fract(frequency*(length(pos)-iGlobalTime))-0.5;
-	float intensity = 1-smoothstep(0.5, 0.5+0.01*frequency, dist);
-	intensity -= (1-smoothstep(0.5-0.11*frequency, 0.5-0.1*frequency, dist));
-	vec3 color = vec3(0.0,intensity,0.0);
-	if(frequency >= 5){
-		color = vec3(0.0,1.0,0.0);
-	}
-	return color;
-}
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 pos = fragCoord.xy/iResolution.xx;
@@ -93,14 +76,16 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     VoronoiResult voronoiResult = voronoi( 16.0*pos );
 	
     // borders	
-	vec3 color = mix(Rings(pos), vec3(0.0), smoothstep( 0.00, 0.04, voronoiResult.borderDist ) );
+	vec3 color = mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist, dist + 0.04, voronoiResult.borderDist ) );
+	color -= mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist - 0.04, dist , voronoiResult.borderDist ) );
 	
-	//ghosting
-	vec2 uv = fragCoord.xy/iResolution.xy;
+	if(dist>0.4){
+		dist = 0.5-dist;
 	
-	color += 0.6 * texture2D(texLastFrame, uv).rgb;
-	color -= 1.0 / 256.0; //dim over time to avoid leftovers
-	color = clamp(color, vec3(0), vec3(1));
+		color = mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist, dist + 0.04, length(voronoiResult.nearestPointDir/vec2(1, sqrt(2))) ) );
+		//color -= mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist - 0.04, dist , length(voronoiResult.nearestPointDir/vec2(1, sqrt(2))) ) );
+	}
+	
 	
 	fragColor = vec4(color,1.0);
 }
@@ -109,3 +94,6 @@ void main()
 {
 	mainImage(gl_FragColor, gl_FragCoord.xy);
 }
+
+
+
