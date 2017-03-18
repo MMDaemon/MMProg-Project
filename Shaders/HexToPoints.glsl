46b75f5160febe2ec8a8070d86ea74078bf522ab
MMDaemon <mettenleitermatthias@web.de>
2017-03-18 15:29:29 +0100

    Added final config

--- Shaders/HexToPoints.glsl
+++ Shaders/HexToPoints.glsl
@@ -1,26 +1,27 @@
 #include "HexagonDistField.glsl"
 
 uniform vec2 iResolution;
-uniform float iGlobalTime;
 uniform float dist;
+uniform float zoom;
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 pos = fragCoord.xy/iResolution.xy;
 	float aspect = iResolution.y/iResolution.x;
-	pos = (2*pos-vec2(1.0,1.0)) * vec2(1,aspect);
+	pos += vec2(-0.005,0.09)*zoom;
+	pos *= 1+((vec2(sqrt(0.66),1)-1)*zoom);
+	pos = (2*(pos)-vec2(1.0,1.0)) * vec2(1,aspect);
 
-    VoronoiResult voronoiResult = voronoi( 8.0*pos );
+    VoronoiResult voronoiResult = voronoi( (8-zoom*2.9)*pos );
 	
     // borders	
 	vec3 color = mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist, dist + 0.04, voronoiResult.borderDist ) );
 	color -= mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist - 0.04, dist , voronoiResult.borderDist ) );
 	
 	if(dist>0.4){
-		dist = 0.5-dist;
-	
-		color = mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist, dist + 0.04, length(voronoiResult.nearestPointDir/vec2(1, sqrt(2))) ) );
-		//color -= mix(vec3(0.0,1.0,0.0), vec3(0.0), smoothstep( dist - 0.04, dist , length(voronoiResult.nearestPointDir/vec2(1, sqrt(2))) ) );
+		dist = 0.5-dist+(zoom*0.145);
+		vec3 pointColor = mix(vec3(0.0,1.0,0.0), vec3(1.0), zoom);
+		color = mix(pointColor, vec3(0.0), smoothstep( dist, dist + 0.04*(1-zoom), length(voronoiResult.nearestPointDir/vec2(1, 1/sqrt(0.66))) ) );
 	}
 	
 	fragColor = vec4(color,1.0);
